3be3f23391b99a4ecfd4bf39ae71c822948ac07f
vigiapriliansyah <apriliansyahvigi@gmail.com>
2023-12-26 17:44:18 +0700

    tambah dan edit sudah di perbaiki

--- app/Controllers/Anime.php
+++ app/Controllers/Anime.php
@@ -49,15 +49,9 @@ class Anime extends BaseController
         if (!$this->validate($validationRules)) {
             return redirect()->to('anime/tambah_anime')->with('errors', "Note : Jangan ada data yang sama atau kosong");
         }
+        $fileVideo->move('uploud/video');
+        $fileGambar->move('uploud/images');
 
-        $namaVideo = $fileVideo->getRandomName();
-        $namaGambar = $fileGambar->getRandomName();
-
-        $fileVideo->move('uploud/video', $namaVideo);
-        $fileGambar->move('uploud/images', $namaGambar);
-
-        $namaVideo = $fileVideo->getName();
-        $namaGambar = $fileGambar->getName();
 
         $this->animeModel->save([
             'judul' => $this->request->getVar('judul'),
@@ -65,8 +59,8 @@ class Anime extends BaseController
             'genre' => $this->request->getVar('genre'),
             'rating' => $this->request->getVar('rating'),
             'tahun' => $this->request->getVar('tahun'),
-            'file_video' => $namaVideo,
-            'file_gambar' => $namaGambar
+            'file_video' => $fileVideo->getName(),
+            'file_gambar' => $fileGambar->getName()
         ]);
 
         return redirect()->to('anime')->with('success', "Berhasil Menguploud Data");
@@ -86,23 +80,30 @@ class Anime extends BaseController
 
     public function update()
     {
-
         $fileVideo = $this->request->getFile('file_video');
         $fileGambar = $this->request->getFile('file_gambar');
 
-
         $id_anime = $this->request->getVar('id_anime');
 
-        $namaVideo = $fileVideo->getRandomName();
-        $namaGambar = $fileGambar->getRandomName();
+        // Jika file video baru disediakan, pindahkan dan dapatkan nama file
+        if ($fileVideo->isValid()) {
+            $fileVideo->move('uploud/video');
+            $namaVideo = $fileVideo->getName();
+        } else {
+            // Jika tidak, gunakan nama file yang sudah ada dalam database
+            $anime = $this->animeModel->data_anime($id_anime);
+            $namaVideo = $anime['file_video'];
+        }
 
-        $fileVideo->move('uploud/video', $namaVideo);
-        $fileGambar->move('uploud/images', $namaGambar);
-
-        $namaVideo = $fileVideo->getName();
-        $namaGambar = $fileGambar->getName();
-
-
+        // Jika file gambar baru disediakan, pindahkan dan dapatkan nama file
+        if ($fileGambar->isValid()) {
+            $fileGambar->move('uploud/images');
+            $namaGambar = $fileGambar->getName();
+        } else {
+            // Jika tidak, gunakan nama file yang sudah ada dalam database
+            $anime = $this->animeModel->data_anime($id_anime);
+            $namaGambar = $anime['file_gambar'];
+        }
 
         $data = [
             'judul' => $this->request->getVar('judul'),
@@ -113,9 +114,11 @@ class Anime extends BaseController
             'file_video' => $namaVideo,
             'file_gambar' => $namaGambar
         ];
+
         $this->animeModel->update_data($data, $id_anime);
         return redirect()->to('anime')->with('success', "Data berhasil diupdate");
     }
+
 
     public function delete($id_anime)
     {
