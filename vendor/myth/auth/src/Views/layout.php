56333a1314827c6468dee46c4e05444896d96256
vigiapriliansyah <apriliansyahvigi@gmail.com>
2023-12-28 11:55:14 +0700

    layout myth auth

--- vendor/myth/auth/src/Views/layout.php
+++ vendor/myth/auth/src/Views/layout.php
@@ -58,7 +58,7 @@
                                 </div>
                             </li>
                             <li class="nav-item" style="padding-top: 9px;">
-                                <a class="navbar-icon text-decoration-none text-white pt-3" href="/dashboard/Anime"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <a class="navbar-icon text-decoration-none text-white pt-3" href="/Anime"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
                                     </svg></a>
