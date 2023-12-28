d586393102969e07a873ab2423690b7f6e21b064
vigiapriliansyah <apriliansyahvigi@gmail.com>
2023-12-28 13:25:12 +0700

    update

--- app/Views/layout/templateHome.php
+++ app/Views/layout/templateHome.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title; ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="<?= base_url('/font/css/all.css'); ?>">
     <link rel="stylesheet" href="<?= base_url('/style/home.css'); ?>">
@@ -14,49 +13,55 @@
 
 <body>
     <!-- --navbar-- -->
-    <nav>
-        <div>
-            <a href="/home"><img src="<?= base_url('/assets/icon.png'); ?>" alt="icon" width="55" height="40"></a>
-        </div>
-        <div class="sidebar" id="sidebar">
-            <i class="fa fa-times" onclick="HideMenu()"></i>
-            <ul>
-                <li>
-                    <a class="nav-link" aria-current="page" href="/home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/home/animeList">Anime List</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/home/genreList">Genre List</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/home/Contact">Contact</a>
-                </li>
-                <li class="login">
-                    <a class="nav-link" href="/Anime">Login</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <div id="search-container">
-                <input type="text" id="search-input" placeholder="Search..." />
-                <button onclick="performSearch()">Search</button>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #1D1B34;">
+        <div class="container-fluid">
+            <div>
+                <a class="navbar-brand" href="/home"><img src="<?= base_url('/assets/icon.png'); ?>" alt="icon" width="55" height="40"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
             </div>
-
-            <button onclick="toggleSearch()">
-                <i class="fas fa-search"></i>
-            </button>
-
-            <!-- Contact Icon (You can replace the src with the path to your contact icon) -->
-            <a href="/Anime" class="contact"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35"
-                    fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                    <path fill-rule="evenodd"
-                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                </svg></a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="/home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/home/animeList">Anime List</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/home/genreList">Genre List</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/home/Contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="d-flex">
+                    <div class="navbar-icons">
+                        <ul class="navbar-nav">
+                            <li class="navbar-item">
+                                <div class="search-box">
+                                    <button class="btn-search">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="25" width="25" viewBox="0 0 512 512">
+                                            <path fill="#ffffff" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                                        </svg>
+                                    </button>
+                                    <input type="text" class="input-search" placeholder="Cari Judul Anime....">
+                                </div>
+                            </li>
+                            <li class="nav-item" style="padding-top: 9px;">
+                                <a class="navbar-icon text-decoration-none text-white pt-3" href="/Anime"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                                    </svg></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <i class="fa fa-bars" onclick="ShowMenu()"></i>
     </nav>
 
 
@@ -111,54 +116,53 @@
     </footer>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
     <script id="dsq-count-scr" src="//website-streaming-anime.disqus.com/count.js" async></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script>
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
+        const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
 
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
 
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
 
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    });
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        });
     </script>
     <script>
-    function toggleSearch() {
-        var searchContainer = document.getElementById("search-container");
-        searchContainer.style.display =
-            searchContainer.style.display === "none" ? "block" : "none";
-    }
+        function toggleSearch() {
+            var searchContainer = document.getElementById("search-container");
+            searchContainer.style.display =
+                searchContainer.style.display === "none" ? "block" : "none";
+        }
 
-    function performSearch() {
-        var searchTerm = document.getElementById("search-input").value;
-        // Perform your search logic here
-        alert("Searching for: " + searchTerm);
-    }
+        function performSearch() {
+            var searchTerm = document.getElementById("search-input").value;
+            // Perform your search logic here
+            alert("Searching for: " + searchTerm);
+        }
 
-    function ShowMenu() {
-        document.getElementById("sidebar").style.top = "0";
-    }
+        function ShowMenu() {
+            document.getElementById("sidebar").style.top = "0";
+        }
 
-    function HideMenu() {
-        document.getElementById("sidebar").style.top = "-100vh";
-    }
+        function HideMenu() {
+            document.getElementById("sidebar").style.top = "-100vh";
+        }
     </script>
 </body>
 
--- vendor/myth/auth/src/Views/layout.php
+++ vendor/myth/auth/src/Views/layout.php
@@ -31,7 +31,7 @@
                 <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="/home">Home</a>
+                            <a class="nav-link active" aria-current="page" href="/home">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/home/animeList">Anime List</a>
