2cb13b75f206badf3163192589de784077eb9d05
SaripNv <sarip0747@gmail.com>
2023-12-27 22:23:23 -0800

    update

--- app/Views/home/genreList.php
+++ app/Views/home/genreList.php
@@ -30,17 +30,18 @@
                         <h1 style="color: white;">Drama</h1>
                         <hr class="bg-danger border-2 border-top border-white" />
                         <li style="color: white;">Chuunibyou demo Koi ga Shitai! Movie: Take On Me</li>
-                        <li style="color: white;">Howl no Ugoku Shiro</li>
                         <li style="color: white;">Evangelion: 3.0+1.0 Thrice Upon a Time</li>
+                        <li style="color: white;">Howl no Ugoku Shiro</li>
                     </div>
                 </div>
                 <div class="p-2 col-5">
                     <div class="col">
                         <h1 style="color: white;">Romance</h1>
                         <hr class="bg-danger border-2 border-top border-white" />
+                        <li style="color: white;">Black Butler: Book of the Atlantic</li>
                         <li style="color: white;">Date A Bullet: Nightmare or Queen</li>
                         <li style="color: white;">Howl no Ugoku Shiro</li>
-                        <li style="color: white;">Black Butler: Book of the Atlantic</li>
+
 
                     </div>
                 </div>
@@ -48,9 +49,9 @@
                     <div class="col">
                         <h1 style="color: white;">Fantasy</h1>
                         <hr class="bg-danger border-2 border-top border-white" />
-                        <li style="color: white;">Howl no Ugoku Shiro</li>
                         <li style="color: white;">Evangelion: 3.0+1.0 Thrice Upon a Time</li>
                         <li style="color: white;">Gintama: The Final</li>
+                        <li style="color: white;">Howl no Ugoku Shiro</li>
                     </div>
                 </div>
                 <div class="p-2 col-5">
@@ -67,8 +68,8 @@
                         <h1 style="color: white;">Shounen</h1>
                         <hr class="bg-danger border-2 border-top border-white" />
                         <li style="color: white;">Ansatsu Kyoushitsu: 365-nIchi no Jikan</li>
-                        <li style="color: white;">Gintama: The Final</li>
                         <li style="color: white;">Date A Bullet: Nightmare or Queen</li>
+                        <li style="color: white;">Gintama: The Final</li>
                     </div>
                 </div>
                 <div class="p-2 col-5">
