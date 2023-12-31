ffe2b87c6ed95ae1b971179beddc1aed25839ba8
vigiapriliansyah <apriliansyahvigi@gmail.com>
2023-12-31 11:15:24 +0700

    contact

--- app/Views/home/contact.php
+++ app/Views/home/contact.php
@@ -10,7 +10,7 @@
         <hr class="bg-info border-2 border-top border-white" />
     </section>
     <h4 style="margin-top: 9rem;">Email : 5NIME@Gmail.com</h4>
-    <h4>Untuk Pertanyaan Seputar Aplikasi :</h4>
+    <h4>Untuk Pertanyaan Seputar Web :</h4>
 
     <h2 style="margin-top: 9rem;"><a class="text-decoration-none text-white" href="https://wa.me/6285657055143" target="_blank">Chat WhatsApp</a></h2>
 </div>
