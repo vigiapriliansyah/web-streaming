8c0151a003d6d1bef9d64ae76bb3ce279ec04b09
vigiapriliansyah <apriliansyahvigi@gmail.com>
2024-01-22 19:53:53 +0700

    d

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -2,9 +2,7 @@
 <?= $this->section('konten'); ?>
 
 
-<!-- <div class="preloader flex-column justify-content-center align-items-center">
-  <img class="animation__wobble" src="<?= base_url('assets/dist/img/AdminLTELogo.png'); ?>" alt="AdminLTELogo" height="60" width="60">
-</div> -->
+
 
 <div class="sidebar">
   <!-- Sidebar user panel (optional) -->
